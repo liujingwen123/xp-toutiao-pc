65d2daeba1025de0774f6ed975ea1772fe6f31c6
jingwen Liu <you@example.com>
2019-12-23 16:20:19 +0800

    使用elment-ui

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
     xp-toutiao-pc
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
 </script>
 
 <style lang="less">
-
 </style>
